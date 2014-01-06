8e4633d168fc804d1c11322f3546fd4973ecef96
HanbinKim <hottuc@naver.com>
2014-01-06 13:21:08 +0900

    p.140  실습완료하였습니다.
    데이트 피커의 카운트 다운 모드 타이머 사용입니다..

--- DateTimePicker/ViewController.m
+++ DateTimePicker/ViewController.m
@@ -9,10 +9,52 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+-(void)stopTimer;
+-(void)startTimer;
 
 @end
 
 @implementation ViewController
+{
+    NSTimer *timer;
+    BOOL timerWorking;
+}
+
+- (void)tickDown : (NSTimer *)timer
+{
+    self.datePicker.countDownDuration = self.datePicker.countDownDuration - 60;
+    
+    if(self.datePicker.countDownDuration <= 60)
+    {
+        NSLog(@"Done!");
+        [self stopTimer];
+    }
+}
+
+- (void) startTimer
+{
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(tickDown:) userInfo:nil repeats:YES];
+}
+
+- (void) stopTimer
+{
+    [timer invalidate];
+    timer = nil;
+    timerWorking = NO;
+}
+
+-(IBAction)toggleTimer:(UIButton *)sender
+{
+    NSString *title = timerWorking ? @"Start" : @"Stop";
+    [sender setTitle:title forState:UIControlStateNormal];
+    timerWorking =! timerWorking;
+    
+    if(timerWorking)
+        [self startTimer];
+    else
+        [self stopTimer];
+}
 
 - (void)viewDidLoad
 {
